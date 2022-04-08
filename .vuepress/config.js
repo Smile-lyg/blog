63238a57fd861881d85861f5805123ad7a3466c7
itsmile <1921243831@qq.com>
2022-04-08 20:17:57 +0800

    配置修改

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,58 +1,66 @@
 module.exports = {
-  "base": '/blog/',
-  "locales": {
+  base: '/blog/',
+  // base: '/',
+  locales: {
     '/': {
-      "lang": 'zh-CN'
-    }
-  },
-  "title": "博客",
-  "description": "记录，成为更好的自己。",
-  "dest": "dist",
-  "head": [
-    ["link", {
-      "rel": "icon",
-      "href": "/favicon.ico"
-    }],
-    ["meta", {
-      "name": "viewport",
-      "content": "width=device-width,initial-scale=1,user-scalable=no"
-    }]
-  ],
-  "theme": "reco",
-  "themeConfig": {
-    "nav": [{
-        "text": "主页",
-        "link": "/",
-        "icon": "reco-home"
-      },
-      {
-        "text": "时间线",
-        "link": "/timeline/",
-        "icon": "reco-date"
-      },
-      {
-        "text": "码云",
-        "icon": "reco-mayun",
-        "link": "https://gitee.com/lai_yingang"
-      },
-      {
-        "text": "GitHub",
-        "icon": "reco-github",
-        "link": "https://github.com/Smile-lyg"
-      }
-    ],
-    "type": "blog",
-    "blogConfig": {
-      "category": {
-        "location": 2,
-        "text": "文章分类"
-      },
-      "tag": {
-        "location": 3,
-        "text": "标签"
-      }
+      lang: 'zh-CN',
     },
-    "friendLink": [
+  },
+  title: '博客',
+  description: '记录，成为更好的自己。',
+  dest: 'dist',
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/favicon.ico',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'viewport',
+        content: 'width=device-width,initial-scale=1,user-scalable=no',
+      },
+    ],
+  ],
+  theme: 'reco',
+  themeConfig: {
+    nav: [
+      {
+        text: '主页',
+        link: '/',
+        icon: 'reco-home',
+      },
+      {
+        text: '时间线',
+        link: '/timeline/',
+        icon: 'reco-date',
+      },
+      {
+        text: '码云',
+        icon: 'reco-mayun',
+        link: 'https://gitee.com/lai_yingang',
+      },
+      {
+        text: 'GitHub',
+        icon: 'reco-github',
+        link: 'https://github.com/Smile-lyg',
+      },
+    ],
+    type: 'blog',
+    blogConfig: {
+      category: {
+        location: 2,
+        text: '文章分类',
+      },
+      tag: {
+        location: 3,
+        text: '标签',
+      },
+    },
+    friendLink: [
       // {
       //   title: 'Torrk\'s Blog',
       //   desc: '记录，成为更好的自己。',
@@ -60,38 +68,38 @@ module.exports = {
       //   link: 'https://conimi.com'
       // }
     ],
-    "logo": "/logo.jpg",
-    "sidebar": 'auto',
-    "subSidebar": 'auto',
-    "search": true,
-    "searchMaxSuggestions": 10,
-    "lastUpdated": "上次更新",
-    "author": "itsmile",
-    "authorAvatar": "/avatar.jpg",
+    logo: '/logo.jpg',
+    sidebar: 'auto',
+    subSidebar: 'auto',
+    search: true,
+    searchMaxSuggestions: 10,
+    lastUpdated: '上次更新',
+    author: 'itsmile',
+    authorAvatar: '/avatar.jpg',
     // "record": "xxxx",
-    "startYear": "2021",
-    "recoLocales": {
+    startYear: '2021',
+    recoLocales: {
       // "homeBlog": {
       //   "article": '文章', // 默认 文章
       //   "tag": '标签', // 默认 标签
       //   "category": '类别', // 默认 分类
       //   "friendLink": '友链' // 默认 友情链接
       // },
-      "pagation": {
-        "prev": '上一页',
-        "next": '下一页',
-        "go": '前往',
-        "jump": '跳转至'
-      }
+      pagation: {
+        prev: '上一页',
+        next: '下一页',
+        go: '前往',
+        jump: '跳转至',
+      },
     },
     valineConfig: {
       appId: 'exgBe08buReYEEENkBp3sTUa-gzGzoHsz', // your appId
       appKey: 'AK4S2shgWknWU3SeEAoVOCWA', // your appKey
-      visitor: true
-    }
+      visitor: true,
+    },
   },
-  "markdown": {
-    "lineNumbers": true
+  markdown: {
+    lineNumbers: true,
   },
   plugins: [
     [
@@ -103,29 +111,47 @@ module.exports = {
       },
     ],
     [
-      "@vuepress-reco/vuepress-plugin-kan-ban-niang",
+      '@vuepress-reco/vuepress-plugin-kan-ban-niang',
       {
-        theme: ['koharu', 'haruto', 'blackCat', 'whiteCat', 'haru1', 'haru2', 'izumi', 'shizuku', 'wanko', 'miku', 'z16'],
+        theme: [
+          'koharu',
+          'haruto',
+          'blackCat',
+          'whiteCat',
+          'haru1',
+          'haru2',
+          'izumi',
+          'shizuku',
+          'wanko',
+          'miku',
+          'z16',
+        ],
         clean: true,
         modelStyle: {
-          position: "fixed",
-          left: "0px",
-          bottom: "0px",
-          opacity: "0.9",
-          zIndex: 99999
-        }
-      }
+          position: 'fixed',
+          left: '0px',
+          bottom: '0px',
+          opacity: '0.9',
+          zIndex: 99999,
+        },
+      },
     ],
-    ["vuepress-plugin-nuggets-style-copy", {
-      copyText: "copy",
-      tip: {
-        content: "复制成功!"
-      }
-    }],
-    ['@vuepress/active-header-links', {
-      sidebarLinkSelector: '.sidebar-link',
-      headerAnchorSelector: '.header-anchor'
-    }],
+    [
+      'vuepress-plugin-nuggets-style-copy',
+      {
+        copyText: 'copy',
+        tip: {
+          content: '复制成功!',
+        },
+      },
+    ],
+    [
+      '@vuepress/active-header-links',
+      {
+        sidebarLinkSelector: '.sidebar-link',
+        headerAnchorSelector: '.header-anchor',
+      },
+    ],
     // ['@vuepress-reco/comments', {
     //   solution: 'valine',
     //   options: {
